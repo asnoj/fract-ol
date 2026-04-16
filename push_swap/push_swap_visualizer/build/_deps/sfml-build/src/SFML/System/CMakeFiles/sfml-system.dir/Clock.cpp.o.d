e41420eba584e20177f0df86a7393fd29f15d045
herisson <jonas.crochet.jc@gmail.com>
2026-04-16 04:51:13 +0200

    Delete push_swap directory

--- push_swap/push_swap_visualizer/build/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.o.d
+++
@@ -1,7 +0,0 @@
-_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.o: \
-  /home/jcrochet/42/projet/push_swap/push_swap_visualizer/build/_deps/sfml-src/src/SFML/System/Clock.cpp \
-  /home/jcrochet/42/projet/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Clock.hpp \
-  /home/jcrochet/42/projet/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Export.hpp \
-  /home/jcrochet/42/projet/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/Config.hpp \
-  /home/jcrochet/42/projet/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Time.hpp \
-  /home/jcrochet/42/projet/push_swap/push_swap_visualizer/build/_deps/sfml-src/src/SFML/System/Unix/ClockImpl.hpp
--- push_swap/push_swap_visualizer/build/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Sleep.cpp.o.d
+++
@@ -1,7 +0,0 @@
-_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Sleep.cpp.o: \
-  /home/jcrochet/42/projet/push_swap/push_swap_visualizer/build/_deps/sfml-src/src/SFML/System/Sleep.cpp \
-  /home/jcrochet/42/projet/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Sleep.hpp \
-  /home/jcrochet/42/projet/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Export.hpp \
-  /home/jcrochet/42/projet/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/Config.hpp \
-  /home/jcrochet/42/projet/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Time.hpp \
-  /home/jcrochet/42/projet/push_swap/push_swap_visualizer/build/_deps/sfml-src/src/SFML/System/Unix/SleepImpl.hpp
